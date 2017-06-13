7e0135742b2bf40fafead68bc1911334755d1970
student04 <smeesathit@gmail.com>
2017-06-13 10:26:23 +0700

    Echo input in log window

--- My Calculate/ViewController.swift
+++ My Calculate/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Implicit varible declaration
+    var intNumber1 = 0
+    var intNumber2: Int = 0
+    var strNumber1 = ""
+    var strNumber2: String = ""
+
+    
+    
+    
+    
+    
+    @IBAction func answerButton(_ sender: Any) {
+        strNumber1 = num1TextField.text!
+        strNumber2 = num2TextField.text!
+        
+        
+        // Echo input values
+        print("strNumber1 ==> \(strNumber1)")
+        print("strNumber2 ==> \(strNumber2)")
+        
+        
+        
+    }   // answerButton listener
+    
+    // Define TextField objects
+    @IBOutlet weak var num1TextField: UITextField!
+    
+    
+    @IBOutlet weak var num2TextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    } // End main method
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    } // End memory management method
 
 
-}
+} // End main class
 
